3eae96e62d69db0905c0137768573eb0f6d31349
imaquej <imaquej@saintemarie-cholet.eu>
2026-03-12 14:16:19 +0100

    Connexion et inscription

---
+++ src/script/Subscription.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+include 'script.php';
+
+if(isset($_POST['email'], $_POST['password'])) {
+
+    $email = htmlspecialchars($_POST['email']);
+    $password = md5(htmlspecialchars($_POST['password']));
+
+    // Requête d'inscription
+    $request_subscribe = $bdd->prepare('INSERT INTO utilisateur (mail, mdp) VALUES(?, ?)');
+    $request_subscribe->execute([$email, $password]);
+
+    echo "<div class='message'>" . "Utilisateur créé... <a href='../../index.html'>" . "Retour à la page d'accueil!" . "</a>" . "</div>";
+}
+
+?>
